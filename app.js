6fb901801039b769f1dc8622b0f00bd23a1ab17e
stepin104310 <69225700+stepin104310@users.noreply.github.com>
2020-09-23 14:11:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 var express = require('express');
 var app = express();
 
-app.get('/home',function(req,res){
-    res.send('Welcome to express');
+var bookList = require("./books.json");
+
+app.get("/showBooks", function(req, res)
+{
+	var match = [];
+    keyword = req.query.author;
+    for (const key in bookList)
+    {
+        if(keyword == bookList[key].author)
+        {
+            match.push(bookList[key]);
+        }
+        else
+        {
+            res.send("Author not found")
+        }
+    }
+    res.send(match);
 })
 
-app.listen(3000,function(){
-    console.log('server listening to the port 3000')
+app.get('/home', function(req,res){
+
+    res.send('welcome to express');
 })
 
-//http://localhost:3000/login/sachin/kolkata
 app.get('/login/:username/:city', function(req,res){
     var uname = req.params.username;
     var ucity = req.params.city;
-    res.send(`Welcome ${uname} from  ${ucity}`);
+    res.send(`Welcome ${uname} from ${ucity}`);
+
 })
 
 app.get('/loginUser/:username/:city', function(req,res){
     var params = req.params;
     res.send(params);
 })
-// to send JSON file
+
 app.get('/userDetails/:username/:city',function(req,res){
-    var user = {"name":uname,"city" :city};
+    var uname = req.params.username;
+    var ucity = req.params.city;
+    var user = {"name":uname,"city":ucity};
     res.send(user);
 })
 
-app.listen('3000', function() {
-    console.log('Server started on port');
+app.get('/register',function(req,res){
+    name = req.query.studentname;
+    id = req.query.studentId;
+    res.send({"stName":name,"stId":id});
 })
 
+app.listen('3000', function(){
+    console.log('server listening to port 3000');
+})
